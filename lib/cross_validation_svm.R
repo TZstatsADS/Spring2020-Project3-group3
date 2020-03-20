552888d017fb2d0a29d017ec880f44edb9360e80
ttd2111 <56269606+ttd2111@users.noreply.github.com>
2020-03-20 03:51:56 -0400

    Create cross_validation_svm.R

---
+++ lib/cross_validation_svm.R
@@ -0,0 +1,27 @@
+svm_cv <- function(dat_train, K, cost){
+  ### Input:
+  ### - train data frame
+  ### - K: a number stands for K-fold CV
+  ### - tuning parameters 
+  
+  n <- dim(dat_train)[1]
+  n.fold <- round(n/K, 0)
+  set.seed(0)
+  s <- sample(rep(1:K, c(rep(n.fold, K-1), n-(K-1)*n.fold)))  
+  cv.error <- rep(NA, K)
+  
+  for (i in 1:K){
+    train.data <- dat_train[s != i,]
+    test.data <- dat_train[s == i,]
+    
+    par <- list(cost = cost)
+    fit <- svm_train(train.data, par)
+    
+    pred <- svm_test(fit, test.data)  
+    error <- mean(pred != test.data$emotion_idx) 
+    print(error)
+    cv.error[i] <- error
+    
+  }			
+  return(c(mean(cv.error),sd(cv.error)))
+}
