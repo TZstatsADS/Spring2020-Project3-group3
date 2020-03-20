5a504ecf9c7efdac336982dc045d63bdec4c3851
ttd2111 <56269606+ttd2111@users.noreply.github.com>
2020-03-20 03:51:59 -0400

    Create test_rf.R

---
+++ lib/test_rf.R
@@ -0,0 +1,11 @@
+test_rf <- function(model, test_df){
+  
+  ### Input: 
+  ###  - the fitted classification model using training data
+  ### (since knn does not need to train, we only specify k here)
+  ###  - processed features from testing images 
+  ### Output: training model specification
+  
+  pred <- predict(model,test_df)
+  return(pred)
+}
